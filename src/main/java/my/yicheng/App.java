10c5451d2a5888ec466f8f415bbf953118ef4122
TanYiCheng1017 <57672894+TanYiCheng1017@users.noreply.github.com>
2021-04-07 11:15:07 +0800

    Initial commit

---
+++ src/main/java/my/yicheng/App.java
@@ -0,0 +1,25 @@
+package my.yicheng;
+
+import javax.swing.*;
+
+public class App extends JFrame
+{
+    public App(){
+        setTitle("Hello World");
+        setSize(600,400);
+        setLayout(null);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(3);
+        setVisible(true);
+
+    }
+
+    public static void main( String[] args )
+    {
+        new App();
+    }
+}
+//
+//<name>HelloWorld</name>
+//<!-- FIXME change it to the project's website -->
+//<url>http://www.example.com</url>
